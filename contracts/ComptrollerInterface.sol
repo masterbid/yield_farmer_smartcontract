f46e75b76d45bc95bd3898f52a5f22e3cef3584d
masterbid <b.iniabasi@gmail.com>
2022-03-11 00:00:34 +0100

    v0.1.0

---
+++ contracts/ComptrollerInterface.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.5.7;
+
+interface ComptrollerInterface {
+  function enterMarkets(address[] calldata cTokens) external returns (uint[] memory);
+  function claimComp(address holder) external;
+  function getCompAddress() external view returns(address);
+}
